e7f52dbded699b4039ffe270fbd00751a4bf76f2
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-10-17 20:36:04 +0530

    Handle CSRF tokens

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"bookings/pkg/config"
 	"bookings/pkg/models"
 	"bookings/pkg/render"
+	"fmt"
 	"net/http"
 )
 
@@ -30,7 +31,7 @@ func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
 	remoteIP := r.RemoteAddr
 	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
 
-	render.RenderTemplate(w, "home.page.html", &models.TemplateData{})
+	render.RenderTemplate(w, r, "home.page.html", &models.TemplateData{})
 }
 
 // About is the about page handler
@@ -41,30 +42,38 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
 	stringMap["remote_ip"] = remoteIP
 
-	render.RenderTemplate(w, "about.page.html", &models.TemplateData{StringMap: stringMap})
+	render.RenderTemplate(w, r, "about.page.html", &models.TemplateData{StringMap: stringMap})
 }
 
 // Reservation renders the make a reservation page and displays form
-func (m *Repository) Reservation(w http.ResponseWriter, r * http.Request) {
-	render.RenderTemplate(w, "make-reservation.page.html", &models.TemplateData{})
+func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "make-reservation.page.html", &models.TemplateData{})
 }
 
 // Generals renders the room page
-func (m *Repository) Generals(w http.ResponseWriter, r * http.Request) {
-	render.RenderTemplate(w, "generals.page.html", &models.TemplateData{})
+func (m *Repository) Generals(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "generals.page.html", &models.TemplateData{})
 }
 
 // Majors renders the room page
-func (m *Repository) Majors(w http.ResponseWriter, r * http.Request) {
-	render.RenderTemplate(w, "majors.page.html", &models.TemplateData{})
+func (m *Repository) Majors(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "majors.page.html", &models.TemplateData{})
 }
 
 // Availability renders the search availability page
-func (m *Repository) Availability(w http.ResponseWriter, r * http.Request) {
-	render.RenderTemplate(w, "search-availability.page.html", &models.TemplateData{})
+func (m *Repository) Availability(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "search-availability.page.html", &models.TemplateData{})
+}
+
+// PostAvailability posts the search availability page content
+func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
+	start := r.Form.Get("start")
+	end := r.Form.Get("end")
+
+	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
 }
 
 // Contact renders the contact page
-func (m *Repository) Contact(w http.ResponseWriter, r * http.Request) {
-	render.RenderTemplate(w, "contact.page.html", &models.TemplateData{})
+func (m *Repository) Contact(w http.ResponseWriter, r *http.Request) {
+	render.RenderTemplate(w, r, "contact.page.html", &models.TemplateData{})
 }
