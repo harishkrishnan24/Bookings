3ca3302ee887647775d39cfb76f493a6bfd5c1af
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-11-03 21:13:21 +0530

    Posting make reservation form

--- internal/forms/forms.go
+++ internal/forms/forms.go
@@ -1,9 +1,15 @@
 package forms
 
 import (
+	"fmt"
 	"net/http"
 	"net/url"
+	"strings"
 )
+
+func (f *Form) Valid() bool {
+	return len(f.Errors) == 0
+}
 
 type Form struct {
 	url.Values
@@ -17,10 +23,29 @@ func New(data url.Values) *Form {
 	}
 }
 
+func (f *Form) Required(fields ...string) {
+	for _, field := range fields {
+		value := f.Get(field)
+		if strings.TrimSpace(value) == "" {
+			f.Errors.Add(field, "This field cannot be blank")
+		}
+	}
+}
+
 func (f *Form) Has(field string, r *http.Request) bool {
 	x := r.Form.Get(field)
 	if x == "" {
 		return false
 	}
+	return true
+}
+
+func (f *Form) MinLength(field string, length int, r *http.Request) bool {
+	x := r.Form.Get(field)
+	if len(x) < length {
+		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
+		return false
+	}
+
 	return true
 }
