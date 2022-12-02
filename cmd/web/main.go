f452788c2fb7fe80d9f3575eb2144c5bf938615b
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-02 19:22:21 +0530

    Add database repository

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bookings/internal/config"
+	"bookings/internal/driver"
 	"bookings/internal/handlers"
 	"bookings/internal/helpers"
 	"bookings/internal/models"
@@ -25,10 +26,11 @@ var errorLog *log.Logger
 
 // main is the main application function
 func main() {
-	err := run()
+	db, err := run()
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer db.SQL.Close()
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
@@ -41,7 +43,7 @@ func main() {
 	log.Fatal(err)
 }
 
-func run() error {
+func run() (*driver.DB, error) {
 	gob.Register(models.Reservation{})
 
 	// Change this to true when in production
@@ -61,18 +63,25 @@ func run() error {
 
 	app.Session = session
 
+	log.Println("Connecting to database...")
+	db, err := driver.ConnectSQL("host=localhost port=5432 dbname=bookings user=postgres password=admin")
+	if err != nil {
+		log.Fatal("Cannot connect to database! Dying...")
+	}
+	log.Println("Connected to the database!")
+
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
 		log.Fatal("cannot create template cache")
-		return err
+		return nil, err
 	}
 
 	app.TemplateCache = tc
 	app.UseCache = false
-	repo := handlers.NewRepo(&app)
+	repo := handlers.NewRepo(&app, db)
 	handlers.NewHandlers(repo)
 	render.NewTemplates(&app)
 	helpers.NewHelpers(&app)
 
-	return nil
+	return db, nil
 }
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,10 +2,13 @@ package handlers
 
 import (
 	"bookings/internal/config"
+	"bookings/internal/driver"
 	"bookings/internal/forms"
 	"bookings/internal/helpers"
 	"bookings/internal/models"
 	"bookings/internal/render"
+	"bookings/internal/repository"
+	"bookings/internal/repository/dbrepo"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -17,11 +20,12 @@ var Repo *Repository
 // Repository is the repository type
 type Repository struct {
 	App *config.AppConfig
+	DB  repository.DatabaseRepo
 }
 
 // NewRepo creates a new repository
-func NewRepo(a *config.AppConfig) *Repository {
-	return &Repository{App: a}
+func NewRepo(a *config.AppConfig, db *driver.DB) *Repository {
+	return &Repository{App: a, DB: dbrepo.NewPostgresRepo(db.SQL, a)}
 }
 
 // NewHandlers sets the repository for the handlers
