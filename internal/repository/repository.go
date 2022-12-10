c9e145409f94120671241ec2983bda51e3afe27c
Harish Krishnan <harishkrishnan1993@gmail.com>
2022-12-10 16:08:42 +0530

    Search availability

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,8 +1,13 @@
 package repository
 
-import "bookings/internal/models"
+import (
+	"bookings/internal/models"
+	"time"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
-	InsertReservation(res models.Reservation) error
+	InsertReservation(res models.Reservation) (int, error)
+	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDates(start, end time.Time, roomID int) (bool, error)
 }
